a0383b16dee44bd1eec6afe23ad10f593deef081
ladislavbielko <ladislav.bielko@gmail.com>
2022-03-19 18:03:44 +0100

    fix temp inception code

--- gan/src/dcgan_models.py
+++ gan/src/dcgan_models.py
@@ -476,30 +476,23 @@ def calculate_inception_score(images, n_split=10, eps=1E-16):
     yhat = model.predict(processed)
     # enumerate splits of images/predictions
     scores = list()
-    n_part = floor(images.shape[0] / n_split)
+    n_part = np.floor(images.shape[0] / n_split)
     for i in range(n_split):
         # retrieve p(y|x)
         ix_start, ix_end = i * n_part, i * n_part + n_part
         p_yx = yhat[ix_start:ix_end]
         # calculate p(y)
-        p_y = expand_dims(p_yx.mean(axis=0), 0)
+        p_y = np.expand_dims(p_yx.mean(axis=0), 0)
         # calculate KL divergence using log probabilities
-        kl_d = p_yx * (log(p_yx + eps) - log(p_y + eps))
+        kl_d = p_yx * (np.log(p_yx + eps) - np.log(p_y + eps))
         # sum over classes
         sum_kl_d = kl_d.sum(axis=1)
         # average over images
-        avg_kl_d = mean(sum_kl_d)
+        avg_kl_d = np.mean(sum_kl_d)
         # undo the log
-        is_score = exp(avg_kl_d)
+        is_score = np.exp(avg_kl_d)
         # store
         scores.append(is_score)
     # average across images
-    is_avg, is_std = mean(scores), std(scores)
+    is_avg, is_std = np.mean(scores), np.std(scores)
     return is_avg, is_std
-
-# pretend to load images
-images = ones((50, 299, 299, 3))
-print('loaded', images.shape)
-# calculate inception score
-is_avg, is_std = calculate_inception_score(images)
-print('score', is_avg, is_std)
